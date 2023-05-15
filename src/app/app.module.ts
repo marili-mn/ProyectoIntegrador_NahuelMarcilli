785208d9e0448f4b19602a28a2a5ae74ef25377a
marili <nahuemarcilli@gmail.com>
2023-05-15 17:00:11 -0300

    profile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
 import { SocialComponent } from './componets/social/social.component';
 import { BannerComponent } from './components/banner/banner.component';
+import { AboutMeComponent } from './componets/about-me/about-me.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { BannerComponent } from './components/banner/banner.component';
     HeaderComponent,
     LogoAPComponent,
     SocialComponent,
-    BannerComponent
+    BannerComponent,
+    AboutMeComponent
   ],
   imports: [
     BrowserModule,
